10d5e0a10eb6461fe6f34a6d8302ee17bb28d82a
Kieran Martin <kieranjmartin@gmail.com>
2017-11-20 21:25:11 +0000

    add tower defence code

--- ball.py
+++ ball.py
@@ -11,10 +11,18 @@ def get_image(path):
                 _image_library[path] = image
         return image
 
+
+
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+max_x = 800
+max_y = 800
+screen = pygame.display.set_mode((max_x, max_y))
 done = False
 clock = pygame.time.Clock()
+x = 30
+y = 30
+mom = 'Still'
+snake_length = 10
 
 while not done:
         for event in pygame.event.get():
@@ -24,6 +32,25 @@ while not done:
         screen.fill((255, 255, 255))
         
         screen.blit(get_image('images/ball.png'), (20, 20))
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]: mom = 'N'
+        if pressed[pygame.K_DOWN]: mom = 'S'
+        if pressed[pygame.K_LEFT]: mom = 'W'
+        if pressed[pygame.K_RIGHT]: mom = 'E'
+        if mom == 'N':
+            y -= 3
+        if mom == 'S':
+            y += 3
+        if mom == 'E':
+            x += 3
+        if mom == 'W':
+            x -= 3    
+        if pressed[pygame.K_ESCAPE]:
+            done = True
+        if x <= 0 or x >= max_x or y <= 0 or y >= max_y:
+            done = True
+        screen.fill((0, 0, 0)) 
+        pygame.draw.rect(screen, (255, 100, 0), pygame.Rect(x, y, 20, 20))
         
         pygame.display.flip()
         clock.tick(60)
--- tower.py
+++ tower.py
@@ -1,12 +1,25 @@
 class graphical_object:
-    def __init__(self, xloc, yloc):
+    def __init__(self, xloc, yloc,
+                 xsize, ysize):
         self.xloc = xloc
-        self.yloc = yloc    
+        self.yloc = yloc
+        self.xsize = xsize
+        self.ysize = ysize
+        
+    def draw(self, screen):
+        import pygame
+        pygame.draw.rect(screen, 
+                         self.color,
+                         pygame.Rect(self.xloc,
+                                     self.yloc,
+                                     self.xsize,
+                                     self.ysize))
 
 class tower(graphical_object):
     range = 10
     power = 1
     health = 1
+    color = (0, 128, 255)
     
     def shoot(self, targets):
         import math
@@ -16,11 +29,70 @@ class tower(graphical_object):
                 choice.health = choice.health - self.power
                 if choice.health > 0:
                     targ_return.append(choice)
-        targ_return            
+        return targ_return            
     
-class attackers:
+class attackers(graphical_object):
     velocity = 1
     health = 10
     power = 1
+    color = (255, 100, 0) 
+    def move(self, targets):
+        targ_return = []
+        if len(targets) == 0:
+            self.yloc = self.yloc + self.velocity
+        else:     
+            for choice in targets:
+                if (choice.xloc <= self.xloc) and ((choice.xloc + choice.xsize) >= self.xloc) and (self.yloc >= choice.yloc) and self.yloc <= (choice.yloc + choice.ysize):
+                    choice.health = choice.health - self.power
+                else:
+                    self.yloc = self.yloc + self.velocity
+                if choice.health > 0:
+                    targ_return.append(choice)
+        return [self, targ_return]
+
+import pygame
+import numpy
+
+pygame.init()
+xmax = 800
+ymax = 600
+screen = pygame.display.set_mode((xmax, ymax))
+done = False
+towers = []
+listofattackers = []
+clock = pygame.time.Clock()
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+                if  pygame.mouse.get_pressed()[0]:
+                    loc = pygame.mouse.get_pos()
+                    towers.append(tower(loc[0], loc[1],
+                                  20,20))
+                if len(listofattackers) < 10:
+                    rand_xloc = numpy.random.randint(xmax)
+                    listofattackers.append(attackers(rand_xloc, 0,
+                                                     20, 20))
+                for shooters in towers:
+                    listofattackers = shooters.shoot(listofattackers)
+                for army in listofattackers:
+                    newvalues = army.move(towers)
+                    army = newvalues[0]
+                    towers = newvalues[1] 
+                screen.fill((0, 0, 0))    
+                for instance in towers:
+                    instance.draw(screen)
+                for instance in listofattackers:
+                    instance.draw(screen)
+                        
+        pygame.display.flip()
+        clock.tick(60)    
+                    
+
+
+
+
+
 
     
